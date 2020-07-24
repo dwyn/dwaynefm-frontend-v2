a2251d3b0e2eba13c7454e3179430ee6574eb380
dwieb <dwayne.harmon@gmail.com>
2020-07-23 20:06:31 -0500

    First Commit -- Did you know black on yellow are the 2 colors with the strongest impact\?

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---packages-core-src-templates-posts-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/core/src/templates/posts.js"))),
+  "component---packages-core-src-templates-post-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/core/src/templates/post.js"))),
+  "component---packages-core-src-templates-collection-category-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/core/src/templates/collection.category.js"))),
+  "component---packages-core-src-templates-collection-tag-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/core/src/templates/collection.tag.js"))),
+  "component---packages-core-src-templates-collection-author-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/core/src/templates/collection.author.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/dwaynefm-frontend-v2/.cache/dev-404-page.js"))),
+  "component---packages-shared-src-pages-404-jsx": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/shared/src/pages/404.jsx"))),
+  "component---packages-shared-src-pages-authors-jsx": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/shared/src/pages/authors.jsx"))),
+  "component---packages-shared-src-pages-contact-jsx": hot(preferDefault(require("/Users/dharmon/Downloads/themeforest-t8Cqheo1-flexiblog-react-gatsby-blog-template-old/@gatsby-theme-flexiblog/packages/shared/src/pages/contact.jsx")))
+}
+
